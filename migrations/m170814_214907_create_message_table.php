e1a14ae3ca350a8915b55e7e3f0d45c4bd85b11a
gavrushovsk <gavrushovsk@bk.ru>
2017-08-15 01:36:05 +0300

    добавлены поля в миграции

--- migrations/m170814_214907_create_message_table.php
+++ migrations/m170814_214907_create_message_table.php
@@ -19,6 +19,7 @@ class m170814_214907_create_message_table extends Migration
             'm_date_registration' => $this->dateTime()->notNull(),
             'm_date_upgrate' => $this->dateTime()->notNull(),
             'rel_user_id' => $this->integer()->notNull(),
+            'm_status' => $this->integer()->notNull()->defaultValue(0),
         ]);
         $this->addForeignKey(message_user_id, message, rel_user_id, user,id);
     }
