f368cacfc573d3c83e9fcd648f4e270955a2df03
gavrushovsk <gavrushovsk@bk.ru>
2017-08-15 02:10:45 +0300

    Созданы модели таблиц user и message

---
+++ models/Message.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "message".
+ *
+ * @property integer $id
+ * @property string $m_title
+ * @property string $m_text
+ * @property string $m_date_registration
+ * @property string $m_date_upgrate
+ * @property integer $m_status
+ * @property integer $rel_user_id
+ *
+ * @property User $relUser
+ */
+class Message extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'message';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['m_title', 'm_text', 'm_date_registration', 'm_date_upgrate', 'rel_user_id'], 'required'],
+            [['m_text'], 'string'],
+            [['m_date_registration', 'm_date_upgrate'], 'safe'],
+            [['m_status', 'rel_user_id'], 'integer'],
+            [['m_title'], 'string', 'max' => 45],
+            [['rel_user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['rel_user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'm_title' => 'M Title',
+            'm_text' => 'M Text',
+            'm_date_registration' => 'M Date Registration',
+            'm_date_upgrate' => 'M Date Upgrate',
+            'm_status' => 'M Status',
+            'rel_user_id' => 'Rel User ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRelUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'rel_user_id']);
+    }
+
+    /**
+     * @inheritdoc
+     * @return MessageQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new MessageQuery(get_called_class());
+    }
+}
