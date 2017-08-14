8c76f161cb882924ceed8f092a24f0c3f4bd02cb
gavrushovsk <gavrushovsk@bk.ru>
2017-08-15 02:10:07 +0300

    Созданы модели таблиц user и message

--- migrations/m170814_214907_create_message_table.php
+++ migrations/m170814_214907_create_message_table.php
@@ -18,10 +18,11 @@ class m170814_214907_create_message_table extends Migration
             'm_text' => $this->text()->notNull(),
             'm_date_registration' => $this->dateTime()->notNull(),
             'm_date_upgrate' => $this->dateTime()->notNull(),
-            'rel_user_id' => $this->integer()->notNull(),
             'm_status' => $this->integer()->notNull()->defaultValue(0),
+            'rel_user_id' => $this->integer()->notNull(),
+
         ]);
-        $this->addForeignKey(message_user_id, message, rel_user_id, user,id);
+        $this->addForeignKey('message_user_id', 'message', 'rel_user_id', 'user','id');
     }
 
     /**
--- models/User.php
+++ models/User.php
@@ -2,103 +2,77 @@
 
 namespace app\models;
 
-class User extends \yii\base\Object implements \yii\web\IdentityInterface
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $surname
+ * @property string $patronymic
+ * @property string $password
+ * @property string $date_registration
+ * @property string $date_update
+ * @property integer $user_role
+ *
+ * @property Message[] $messages
+ */
+class User extends \yii\db\ActiveRecord
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
-
     /**
      * @inheritdoc
      */
-    public static function findIdentity($id)
+    public static function tableName()
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return 'user';
     }
 
     /**
      * @inheritdoc
      */
-    public static function findIdentityByAccessToken($token, $type = null)
+    public function rules()
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
-    {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return [
+            [['name', 'surname', 'patronymic', 'password', 'date_registration', 'date_update'], 'required'],
+            [['date_registration', 'date_update'], 'safe'],
+            [['user_role'], 'integer'],
+            [['name', 'surname', 'patronymic'], 'string', 'max' => 45],
+            [['password'], 'string', 'max' => 100],
+        ];
     }
 
     /**
      * @inheritdoc
      */
-    public function getId()
+    public function attributeLabels()
     {
-        return $this->id;
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'surname' => 'Surname',
+            'patronymic' => 'Patronymic',
+            'password' => 'Password',
+            'date_registration' => 'Date Registration',
+            'date_update' => 'Date Update',
+            'user_role' => 'User Role',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMessages()
+    {
+        return $this->hasMany(Message::className(), ['rel_user_id' => 'id']);
     }
 
     /**
      * @inheritdoc
+     * @return UserQuery the active query used by this AR class.
      */
-    public function getAuthKey()
+    public static function find()
     {
-        return $this->authKey;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        return $this->password === $password;
+        return new UserQuery(get_called_class());
     }
 }
